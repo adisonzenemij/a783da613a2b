4f22427ec35d17958a1eed1e9f57344f4cd8833e
Adison Jimenez <adisonzenemij@gmail.com>
2026-08-22 23:21:36 -0500

    feat: 4eb4aa65-5074-4004-b27d-2ec012968d0e

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: 'working/**',
     renderMode: RenderMode.Client,
   },
